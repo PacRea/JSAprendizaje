4376b384d388a150007b039056173cc90c140161
Diego Rea Morales <diegoream23@gmail.com>
2025-09-10 20:27:18 -0600

    01 listo

---
+++ Basic/01-varibales.js
@@ -0,0 +1,20 @@
+// var
+
+var helloWorld = "!Hello World!"
+console.log(helloWorld)
+
+helloWorld = "Hola de nuevo mundo"
+console.log(helloWorld)
+
+// let
+
+let helloWorld2 = "Hola Mundo 2"
+console.log(helloWorld2)
+
+helloWorld2 = "Hola de nuevo mundo 2"
+console.log(helloWorld2)
+
+// const
+
+const helloWord3 = "Hola mundo 3"
+console.log(helloWord3)
