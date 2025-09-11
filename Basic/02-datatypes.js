c736c5bfa0a37f7d4ce20a6f92fc329e6382a719
Diego Rea Morales <diegoream23@gmail.com>
2025-09-10 20:49:15 -0600

    02 inciada

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,13 @@
+let name = "Diego Rea "
+let alias = "PacRea "
+let email = "diego@correo.com"
+
+let age = 22
+let height = 1.72
+
+let isStudent = true
+let isTeacher = false
+
+let undefinedValue
+
+console.log(undefinedValue)
