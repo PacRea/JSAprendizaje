34c3622f312ac279387b8422e9d1dfd7560a6855
Diego Rea Morales <diegoream23@gmail.com>
2025-09-13 16:07:36 -0600

    02 lista, tipos de datos

--- Basic/02-datatypes.js
+++ Basic/02-datatypes.js
@@ -1,13 +1,27 @@
+// Strings
 let name = "Diego Rea "
 let alias = "PacRea "
 let email = "diego@correo.com"
 
+// Numbers
 let age = 22
 let height = 1.72
 
+// Booleans
 let isStudent = true
 let isTeacher = false
 
+// Undefined 
 let undefinedValue
-
 console.log(undefinedValue)
+
+// Null 
+let nullValue = null
+console.log(nullValue)
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+//BigInt
+let myBigInt = BigInt(1222222222222222222222222222222222222)
+let myBigInt2 = 1222222222222222222222222222222222222n
