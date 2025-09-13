a593667c2e84c93efb507ce64fdb6791af1f07a9
Diego Rea Morales <diegoream23@gmail.com>
2025-09-13 16:42:01 -0600

    03 iniciado

---
+++ Basic/03-beginner-exercise.js
@@ -0,0 +1,37 @@
+// Este es un comentario en una sola línea 
+
+/*
+ Estos 
+ son comentarios 
+ varias lineas
+ */
+
+ let num1 = 15
+
+ let nombre = "Diego"
+
+ let mayorEdad = true
+
+ let valorSinAsignar
+
+ let valor = null
+
+ let mySymbol = Symbol("pase")
+
+ let numGrande = 12948128941729481n
+
+ console.log(num1)
+ console.log(nombre)
+ console.log(mayorEdad)
+ console.log(valorSinAsignar)
+ console.log(valor)
+ console.log(mySymbol)
+ console.log(numGrande)
+
+ console.log(typeof num1)
+ console.log(typeof nombre)
+ console.log(typeof mayorEdad)
+ console.log(typeof valorSinAsignar)
+ console.log(typeof valor)
+ console.log(typeof mySymbol)
+ console.log(typeof numGrande)
